b651f20b5c2e3816aeea1d0b7d0ddb0ba656dbbf
gavin <102251937@qq.com>
2022-07-18 00:21:03 +0800

    volatile不能保证原子性

---
+++ src/test/java/com/struggle/StruggleApplicationTests.java
@@ -0,0 +1,14 @@
+package com.struggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StruggleApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
