4245f0547f3943da6a6e509830e65f7dd7110f09
sly <2514526166@qq.comcom>
2022-07-21 00:33:24 +0800

    实现类--专属会员优惠方式

---
+++ src/test/java/com/struggle/design/strategy/ExclusiveVIP.java
@@ -0,0 +1,24 @@
+package com.struggle.design.strategy;
+
+import com.struggle.design.strategy.Buyer;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+/**
+ * @Author sly
+ * @Date 2022/7/20 23:47
+ * @Description 实现类--专属会员优惠方式
+ */
+@Service(value = "ExclusiveVIP")
+public class ExclusiveVIP implements Buyer {
+    @Override
+    public String getName() {
+        return "ExclusiveVIP";
+    }
+
+    @Override
+    public BigDecimal calPrice(BigDecimal orderPrice) {
+        return orderPrice.multiply(new BigDecimal(0.7));
+    }
+}
