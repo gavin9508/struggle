a551be035fdffabdbca3175662f31278d2e6a1a4
wangkl <wangkl@123.com>
2022-07-28 23:21:25 +0800

    GC

---
+++ src/test/java/com/struggle/object/gc/GCDemo1.java
@@ -0,0 +1,17 @@
+package com.struggle.object.gc;
+
+/**
+ * -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:./gc.log
+ *
+ * 为了更清晰的查看各个区的垃圾回收情况，可以使用jstat命令
+ * jstat -gc <pid> <period> <count>  来监控垃圾回收
+ *     （pid为Java进程的id，period指每次监控之间的时间间隔(s)，count指监控次数）
+ */
+public class GCDemo1 {
+    public static void main(String[] args) {
+        String str = "abc";
+        while (true){
+            str = str.concat("abc");
+        }
+    }
+}
